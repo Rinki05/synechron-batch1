28826e6e44de4ca3f5334cc33a6beed5ebb42611
Rinki05 <45780075+Rinki05@users.noreply.github.com>
2018-12-12 14:50:17 +0530

    updated jenkins.sh in cc-avenue

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "[INFO] Running Job"
+echo "[INFO] Running Job for branch cc-avenue"
 echo "[INFO] File updated for Poll after Jenkins configuration"
 echo "[INFO]Hello Jenkins from cc-avenue"
